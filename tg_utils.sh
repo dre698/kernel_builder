ceaa600063159af5b0b3903c472637c777cbb4fa
dre <134717165+dre698@users.noreply.github.com>
2026-09-13 17:14:40 +0700

    Fix curl command for Telegram API in tg_utils.sh

--- tg_utils.sh
+++ tg_utils.sh
@@ -6,12 +6,14 @@ case $1 in
   msg)
     curl -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
       -d chat_id="${CHAT_ID}" \
+      ${THREAD_ID:+-d message_thread_id="${THREAD_ID}"} \
       -d "disable_web_page_preview=true" \
       -d "parse_mode=html" \
       -d text="$(echo "$2" | sed 's/%nl/\n/g')"
   ;;
   up | upload)
     curl -F chat_id="${CHAT_ID}" \
+      ${THREAD_ID:+-F message_thread_id="${THREAD_ID}"} \
       -F document=@"$2" \
       -F parse_mode=html https://api.telegram.org/bot${BOT_TOKEN}/sendDocument \
       -F caption="$(echo "$3" | sed 's/%nl/\n/g')"
